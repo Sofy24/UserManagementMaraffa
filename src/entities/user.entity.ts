da3c095ecaa1f0580c1526d25f28bac3f798514a
Matteo Santoro <m.santoro@cosmobile.com>
2024-01-26 15:45:11 +0100

    feat: refined user

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,17 +1,41 @@
-import { Entity, Column, OneToMany, PrimaryColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { UUID } from 'typeorm/driver/mongodb/bson.typings';
 
 @Entity()
 export class User {
-  @PrimaryColumn('uuid')
-  id: UUID;
+  @PrimaryGeneratedColumn('uuid')
+  id?: UUID;
 
   @Column()
-  name: string;
+  @ApiProperty()
+  nickname: string;
 
   @Column()
-  lastName: string;
+  @ApiProperty()
+  password: string;
+
+  @Column({ nullable: true })
+  @ApiProperty()
+  email?: string;
 
   @Column({ default: true })
-  isActive: boolean;
+  @ApiProperty()
+  isActive?: boolean;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  @ApiProperty()
+  registrationDate?: Date;
+
+  @Column()
+  @ApiProperty({ default: 0 })
+  gamesPlayed?: number;
+
+  @Column()
+  @ApiProperty({ default: 0 })
+  gamesWon?: number;
+
+  @Column()
+  @ApiProperty({ default: 0 })
+  criccaNum?: number;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
-import { User } from 'src/entities/user.entity';
+import { User } from '../entities/user.entity';
 import { UserService } from './user.service';
 
 @Crud({
