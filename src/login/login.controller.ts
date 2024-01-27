d1c0cf528afe4a15abea4ddc61054faca8dc9818
Matteo Santoro <m.santoro@cosmobile.com>
2024-01-27 13:32:46 +0100

    fix: fiexed the mechanics of login

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginService } from './login.service';
 import { Response } from 'express';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Login')
 @Controller('login')
 export class LoginController {
   constructor(
@@ -10,15 +13,25 @@ export class LoginController {
     private readonly userService: UserService,
   ) {}
 
-  @Get()
-  public async login(@Body() body: any, @Res() res: Response) {
-    console.log(body);
+  @Post()
+  @ApiOperation({ summary: 'Does the authentication' })
+  @ApiBody({
+    description: 'Data for autentication',
+    type: LoginDto, // Sostituisci con il DTO effettivo per l'autenticazione
+  })
+  @ApiResponse({ status: 200, description: 'Authentication successfull' })
+  @ApiResponse({ status: 401, description: 'Credentials are incorrect' })
+  public async login(@Body() body: LoginDto, @Res() res: Response) {
     const user = await this.userService.findOne({
       where: { nickname: body.nickname },
       select: { nickname: true, password: true },
     });
+    const authenticated = await this.loginService.validateUser(
+      body.password,
+      user,
+    );
     res
-      .status((await this.loginService.validateUser(body, user)) ? 200 : 401)
-      .send();
+      .status(authenticated ? 200 : 401)
+      .send(authenticated ? { message: 'ok' } : { message: 'ko' });
   }
 }
